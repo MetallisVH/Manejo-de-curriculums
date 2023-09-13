c254957f58198dbcdf49903223745d51786a631a
MetallisVH <trabajosdelaunap@gmail.com>
2023-09-13 15:52:08 -0300

    confidguracion de paths

--- Manejo_curriculums/App_curriculums/urls.py
+++ Manejo_curriculums/App_curriculums/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('/', views.Index_page, name = 'Index_page'),
     path('/Home/', views.Home_page, name = 'Home_page'),
+    path('/Home/emplogin/', views.Login_empleado, name= 'Login_empleado'),
+    path('/Home/contrlogin/', views.Login_empleador, name = 'Login_empleador'),
+    path('/Home/regemp/', views.Register_empelado, name = 'Register_empleado'),
+    path('/Home/regcontr/', views.Register_empleador, name = 'Register_empleador')
 ]
--- Manejo_curriculums/App_curriculums/views.py
+++ Manejo_curriculums/App_curriculums/views.py
@@ -5,3 +5,15 @@ def Index_page(request):
 
 def Home_page(request):
     return render(request, 'html/Home.html')
+
+def Login_empleado(request):
+    return render(request, 'html/inicio_empleado.html')
+
+def Login_empleador(request):
+    return render(request, 'html/inicio_empleador.html')
+
+def Register_empelado(request):
+    return render(request, 'html/Registro_empleado.html')
+
+def Register_empleador(request):
+    return render(request, 'html/Registro_empleador.html')
