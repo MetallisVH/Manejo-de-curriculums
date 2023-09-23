ca37538bad8eeb5c658f2c2fce0db2133817eadf
MetallisVH <trabajosdelaunap@gmail.com>
2023-09-23 16:20:31 -0300

    pagina de registro exitoso + logica de registro

--- Manejo_curriculums/App_curriculums/views.py
+++ Manejo_curriculums/App_curriculums/views.py
@@ -25,6 +25,7 @@ def Register_empelado(request):
 def Register_empleador(request):
     return render(request, 'html/Registro_empleador.html')
 
+
 def registro_empleado(request):
     if request.method == 'POST':
         form = RegistroEmpleadoForm(request.POST)
@@ -33,6 +34,12 @@ def registro_empleado(request):
             fecha_nacimiento = form.cleaned_data['fechaNacimiento']
             año_actual = date.today().year
             edad = año_actual - fecha_nacimiento.year - ((date.today().month, date.today().day) < (fecha_nacimiento.month, fecha_nacimiento.day))
+            
+            genero = form.cleaned_data['genero']
+            
+            if genero == 'otro':
+                # Toma el valor ingresado en el campo 'otroGenero'
+                genero = form.cleaned_data['otroGenero']
 
             # Crea una instancia del modelo Usuarios y asigna los valores del formulario
             usuario = Usuarios(
@@ -41,8 +48,9 @@ def registro_empleado(request):
                 contrasena=make_password(form.cleaned_data['contrasena']),
                 telefono=form.cleaned_data['telefono'],
                 fecha_nac=form.cleaned_data['fechaNacimiento'],
-                genero=form.cleaned_data['genero'],
+                genero=genero,
                 edad=edad,  # Asigna la edad calculada
+                nivel_cuenta = 0
                 # Asigna otros campos del modelo Usuarios según sea necesario
             )
             usuario.save()  # Guarda el usuario en la base de datos
