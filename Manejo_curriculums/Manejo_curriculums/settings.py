064f7e11539d6ee98d684fd6b5e4c074a1f65618
MetallisVH <trabajosdelaunap@gmail.com>
2023-09-24 00:54:04 -0300

    cache al gitignore + arreglo en credenciales

--- Manejo_curriculums/Manejo_curriculums/settings.py
+++ Manejo_curriculums/Manejo_curriculums/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from config import EMAIL_HOST_Usuario, EMAIL_HOST_App_password
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,3 +125,9 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = EMAIL_HOST_Usuario
+EMAIL_HOST_PASSWORD = EMAIL_HOST_App_password
