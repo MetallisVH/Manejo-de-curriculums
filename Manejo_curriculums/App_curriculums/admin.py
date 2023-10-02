c199beeb81ea672072ef64e00afb4ff1ad39c940
MetallisVH <trabajosdelaunap@gmail.com>
2023-10-02 16:55:55 -0300

    arreglo en admin para ver las educaciones ingresadas

--- Manejo_curriculums/App_curriculums/admin.py
+++ Manejo_curriculums/App_curriculums/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Usuarios  # Importa el modelo
+from .models import Usuarios,Educaciones  # Importa el modelo
 
 # Registra el modelo para que aparezca en el sitio admin
 admin.site.register(Usuarios)
+admin.site.register(Educaciones)
