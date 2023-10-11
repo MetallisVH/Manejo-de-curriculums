df37a60343e69c6c6f67d2928552c3e41c2bccd0
Joreyesi <joel_ranger@hotmail.com>
2023-10-11 19:34:09 -0300

    update class usuario

--- Manejo_curriculums/App_curriculums/models.py
+++ Manejo_curriculums/App_curriculums/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 class Usuarios(models.Model):
-    id = models.AutoField(primary_key=True, null=True,unique=True)
+    id = models.AutoField(primary_key=True, null=False,unique=True)
     rut = models.IntegerField(null=True, blank=True,unique=True)
     dv = models.CharField(max_length=1, null=True, blank=True)
     direccion = models.CharField(max_length=54,null=True, blank=True)
